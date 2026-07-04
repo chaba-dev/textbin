dcd2560b119a00f89351474f036adfc263be3228
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-03 23:47:48 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Copy & Paste!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Elixir
+            beam.packages.erlang_28.elixir
+            beam.packages.erlang_28.rebar3
+            erlang_28
+
+            # Golang
+            go
+            golangci-lint
+            gotests
+            gomodifytags
+            gore
+            gotools
+            delve
+
+            # LSPs
+            beamPackages.expert
+            erlang-language-platform
+            gopls
+
+            # Tools
+            yamllint
+            shfmt
+            shellcheck
+            git-cliff
+            yaml-language-server
+            goreleaser
+          ];
+
+          shellHook = ''
+            export MIX_HOME="$PWD/.nix/mix"
+            export HEX_HOME="$PWD/.nix/hex"
+            export REBAR_CACHE_DIR="$PWD/.nix/rebar3"
+            export ERL_AFLAGS="-kernel shell_history enabled"
+
+            mkdir -p "$MIX_HOME" "$HEX_HOME" "$REBAR_CACHE_DIR"
+          '';
+
+        };
+      });
+}
