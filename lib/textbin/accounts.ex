e9ec39dfb1fc0aa29f062ec72cd1843715027a48
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-24 00:55:43 -0700

    allow creating guest users

--- lib/textbin/accounts.ex
+++ lib/textbin/accounts.ex
@@ -60,6 +60,15 @@ defmodule Textbin.Accounts do
   """
   def get_user!(id), do: Repo.get!(User, id)
 
+  def get_guest_user(id) when is_binary(id) do
+    case Ecto.UUID.cast(id) do
+      {:ok, id} -> Repo.get_by(User, id: id, kind: "guest")
+      :error -> nil
+    end
+  end
+
+  def get_guest_user(_id), do: nil
+
   ## User registration
 
   @doc """
@@ -78,6 +87,25 @@ defmodule Textbin.Accounts do
     %User{}
     |> User.email_changeset(attrs)
     |> Repo.insert()
+  end
+
+  def create_guest_user(attrs \\ %{}) do
+    attrs =
+      attrs
+      |> Map.put_new("email", guest_email())
+      |> Map.put_new("default_paste_ttl", guest_paste_ttl())
+
+    %User{}
+    |> User.guest_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  defp guest_email do
+    "guest-#{Ecto.UUID.generate()}@guest.textbin.local"
+  end
+
+  defp guest_paste_ttl do
+    Application.get_env(:textbin, :guest_paste_ttl, "6h")
   end
 
   ## Settings
